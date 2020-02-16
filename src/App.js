12a76456cc7c15e432da253782f454be7ab2e0cd
Girts Rimis <girts60@gmail.com>
2020-02-16 17:04:34 +0200

    Added images and did some cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import LandingPage from './components/LandingPage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mājas lapa, kas ir izveidota!</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+  render()
+  {
+    return (
+      <div className="App">
+        <LandingPage />
+      </div>      
+    )
+  }
 }
 
 export default App;
---
+++ src/components/LandingPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class LandingPage extends Component 
+{
+    render()
+    {
+      return (
+        <div>
+          <nav>
+              <ul>
+                  <li>
+                      <a href="#">Home</a>
+                  </li>
+              </ul>
+          </nav>
+        </div>      
+      )
+    }
+  }
+
+export default LandingPage
