1ee69a651f1b9beb00bbe881d11d532daba0bfcc
Girts Rimis <girts60@gmail.com>
2020-02-16 18:59:35 +0200

    Added navbar with effects

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
+import logo from './logo_transparent.png';
+import './sass/LandingPage.sass'
 
 export class LandingPage extends Component 
 {
     render()
     {
       return (
-        <div>
+        <div className="picture">
           <nav>
-              <ul>
+              <ul className="nav-grid">
                   <li>
-                      <a href="#">Home</a>
+                      <a href="#top">
+                          <img src={logo} alt="Logo" />
+                      </a>
+                  </li>
+                  <li>
+                      <a className="underline" href="#top">Par mums</a>
+                  </li>
+                  <li>
+                      <a className="underline" href="#top">Peidāvājumi</a>
+                  </li>
+                  <li>
+                      <a className="underline" href="#top">Portfolio</a>
+                  </li>
+                  <li>
+                      <a className="underline" href="#top">Kontakti</a>
                   </li>
               </ul>
           </nav>
         </div>      
       )
     }
-  }
+}
 
 export default LandingPage
