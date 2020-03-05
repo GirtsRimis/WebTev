331c182bf3cbb5591a9031cd32bf2d7a9461fac4
Girts Rimis <girts60@gmail.com>
2020-03-05 09:25:57 +0200

    Installed express

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ export default class Form extends Component {
     render() {
         return (
             <div className="form">
-                <form>
+                <form action="/action.php" method="POST">
                     <input className="form-input" type="text" name="e-mail"  placeholder="piem. 'example@gmail.com'" />
                     <br></br>
                     <input className="submit" type="submit" value="Sūtīt" />
